7022d11761f1ab8fe8be522693b934238ddc4a75
yqb <798046397@qq.com>
2019-02-17 10:02:46 +0800

    适配器模式

---
+++ src/Study/designMode/adapte/Center.java
@@ -0,0 +1,21 @@
+package Study.designMode.adapte;
+
+/**
+ * @version 1.0
+ * @date 2019/1/31 15:18
+ */
+public class Center extends Player {
+    public Center(String name) {
+        super(name);
+    }
+
+    @Override
+    public void attrack() {
+        System.out.println(String.format("中锋%s进攻",name));
+    }
+
+    @Override
+    public void defense() {
+        System.out.println(String.format("中锋%s防守",name));
+    }
+}
